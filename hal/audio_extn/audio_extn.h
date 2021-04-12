a8fd660b1f2c460c008b5e6983f9f30dc3986718
jhonboy121 <alfredmathew05@gmail.com>
2021-04-12 14:03:11 +0530

    Revert "audio_extn: define set,get_metadata if USE_VENDOR_EXTN is enabled"
    This reverts commit 947b97887def01e0ab31f12634c28da1d6ffb283.

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -104,7 +104,7 @@ int audio_extn_parse_compress_metadata(struct stream_out *out,
 #define AUDIO_OUTPUT_BIT_WIDTH ((config->offload_info.bit_width == 32) ? 24\
                                    :config->offload_info.bit_width)
 
-#ifdef USE_VENDOR_EXTN
+#ifndef ENABLE_EXTENDED_COMPRESS_FORMAT
 #define compress_set_metadata(compress, metadata) (0)
 #define compress_get_metadata(compress, metadata) (0)
 #define compress_set_next_track_param(compress, codec_options) (0)
